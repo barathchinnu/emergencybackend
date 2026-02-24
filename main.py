6f6d5fcffba7b52380ddebf5f4a8c62b38f8f0e2
barathchinnu <barathchinnu5@gmail.com>
2026-02-24 09:10:25 +0530

    Allow all origins for Vercel

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if allowed_origins_env:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
